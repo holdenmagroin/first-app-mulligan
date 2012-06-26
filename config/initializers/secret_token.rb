f6a77019c7a2431824eb21b782f8dfa784762e0c
JP <jdasher5@gmail.com>
2012-06-26 17:00:15 -0400

    Initial commit...again!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstAppMulligan::Application.config.secret_token = 'e33c7885de72ed01ccb461ec70a0e31e43615ce71ed62ce6143c78666239a954d73cda5b9ff5717c0f6358a9b0f98cdec4fc6c4144e13a28bccbe9eb075061e0'
